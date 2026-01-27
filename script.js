bcc1bd3e957cd152bd5e8bb973677912e75e9302
liqshg <liza.tutak@gmail.com>
2026-01-27 17:58:16 +0200

    6

--- script.js
+++ script.js
@@ -51,3 +51,20 @@
   //     }
   //   ]
   // });
+
+  const scrollBtn = document.getElementById('scrollTopBtn');
+
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 300) {
+      scrollBtn.classList.add('show');
+    } else {
+      scrollBtn.classList.remove('show');
+    }
+  });
+
+  scrollBtn.addEventListener('click', () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  });
